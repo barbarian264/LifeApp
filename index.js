d4707af75feb4a2bd702aef55529c2ffd3635ae2
Jorge Barbarian <jorgebarbarian@gmail.com>
2018-01-13 11:45:13 -0600

    change

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var express = require("express");
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 8000;
 
 var bodyParser = require("body-parser");
 
@@ -30,6 +31,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening');
 });
